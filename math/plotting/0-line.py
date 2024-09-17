5a6185d15b718ae7674e38921445809fbdc8a351
MrNeonn75 <devneonn75@gmail.com>
2024-09-17 19:35:07 +0400

    bug fix

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,9 +2,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def line():
-    y = np.arange(0, 11) ** 3
 
+def line():
+    """
+    Complete the following source code to plot y as a line graph:
+
+    y should be plotted as a solid red line
+    The x-axis should range from 0 to 10
+    """
+    y = np.arange(0, 11) ** 3
 
     plt.xlim(0, 10)
     plt.plot(y, color='red')
--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -2,7 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def scatter():
+    """
+    Complete the following source code to plot x ↦ y as a scatter plot:
+
+    The x-axis should be labeled Height (in)
+    The y-axis should be labeled Weight (lbs)
+    The title should be Men's Height vs Weight
+    The data should be plotted as magenta points
+    """
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
