b66b925cd5c53b7a4c8ea17d3f833f9a8794ebf2
MrNeonn75 <devneonn75@gmail.com>
2025-06-15 23:26:30 +0400

    4

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -15,14 +15,6 @@ def matrix_transpose(matrix):
     if (not isinstance(matrix[0], list)):
         return [len(matrix)]
     else:
-        
-        trans_mat = []
-        tmp =[]
-        for i in range(len(matrix[0])):
-            for list in matrix:
-                tmp.append(list[i])
-            trans_mat.append(tmp)
-            tmp = []
-    
-    return trans_mat
-
+        new_matrix = [[matrix[j][i] for j in range(len(matrix))]
+                      for i in range(len(matrix[0]))]
+    return new_matrix
